5da62d834d6496262173fa655a6371b958673c28
Nico vélez <arangonicovelez@gmail.com>
2025-11-04 21:52:05 -0500

    Added try/catch using and the user description when viewing following or followers lists

--- App.js
+++ App.js
@@ -1,78 +1,64 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import ViewProfileScreen from './src/screens/ViewProfileScreen';
-import FollowersListScreen from './src/screens/FollowersListScreen';
-import FollowingListScreen from './src/screens/FollowingListScreen';
+import { PaperProvider } from "react-native-paper";
+
+import ViewProfileScreen from './src/Screens/ViewProfileScreen';
+import FollowersListScreen from './src/Screens/FollowersListScreen';
+import FollowingListScreen from './src/Screens/FollowingListScreen';
+
+import ZHeader from './src/Components/ZHeader';//from marsiglia code
+import PublishPost from './src/Screens/PublishPost';//from marsiglia code
+import Feed from './src/Screens/Feed';//from marsiglia code
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
 const easyExampleProfile = {
     id: 1,
-    name: 'Laura',
-    lastName: 'Shigihara',
-    username: 'lauraSH',
+    fullName: 'Laura Shigihara',
+    userName: 'lauraSH',
     email: 'laura@pvz.com',
-    phone: '1234567890',
+    password: '1234567890',
     description: 'Singer and composer of “Zombie on Your Lawn” from PvZ and George’s girlfriend',
     avatarUrl: '',
-    followers: [
-        {
-            id: 2,
-            name: 'George',
-            lastName: 'Fan',
-            username: 'georgeFan',
-            email: 'george@pvz.com',
-            description: 'Creator of Plants vs Zombies and Laura’s boyfriend',
-            avatarUrl: '',
-            followers: [],
-            following: [],
-        },
-        {
-            id: 3,
-            name: 'Marsiglia',
-            lastName: 'Davila',
-            username: 'marsiDalej',
-            email: 'marsiDalej@gmail.com',
-            description: 'Hater of Nicolas’s interfaces',
-            avatarUrl: '',
-            followers: [],
-            following: [],
-        },
-    ],
-    following: [
-        {
-            id: 4,
-            name: 'Luis',
-            lastName: 'Flores',
-            username: 'Fernanfloo',
-            email: 'fernanfloo@yt.com',
-            description: 'Youtube legend',
-            avatarUrl: '',
-            followers: [],
-            following: [],
-        },
-    ],
+    followers: [{
+        id: 3,fullName: 'marsiglia', userName: 'follower1', description: ''
+    }],
+    following: [{id: 2,fullName: 'marsiglia', userName: 'follower1', description: 'I love your music!'}],
 };
 
 return (
-    <NavigationContainer>
-        <Stack.Navigator>
-            <Stack.Screen
-                name="ViewProfile"
-                component={ViewProfileScreen}
-                initialParams={{ profile1: easyExampleProfile }}
-            />
-            <Stack.Screen
-                name="FollowersList"
-                component={FollowersListScreen}
-            />
-            <Stack.Screen
-                name="FollowingList"
-                component={FollowingListScreen}
-            />
-        </Stack.Navigator>
-    </NavigationContainer>
+    <PaperProvider>
+        <NavigationContainer>
+        <ZHeader username = {"username?"}/> //from marsiglia code
+            <Stack.Navigator initialRouteName="Feed">
+                <Stack.Screen//from marsiglia code
+                    name="Feed"
+                    component={Feed}
+                    options={{ headerShown: false }}
+                />
+                <Stack.Screen//from marsiglia code
+                    name="PublishPost"
+                    component={PublishPost}
+                    options={{ headerShown: false }}
+                    initialParams={{username: "username?"}}
+                />
+                <Stack.Screen
+                    name="ViewProfile"
+                    component={ViewProfileScreen}
+                    initialParams={{ profile1: easyExampleProfile }}
+                />
+                <Stack.Screen
+                    name="FollowersList"
+                    component={FollowersListScreen}
+                />
+                <Stack.Screen
+                    name="FollowingList"
+                    component={FollowingListScreen}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    </PaperProvider>
 );
 }
